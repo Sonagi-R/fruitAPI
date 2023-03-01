bb78874011a99ea7597aa74470da6a08e8a57446
Lewis Takavarasha <lewis.takavarasha@gmail.com>
2023-03-01 17:40:57 +0000

    add create and delete fruit inputs

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
-require('dotenv').config()
-const apiKey = process.env.APIKEY
+// require('dotenv').config()
+// const apiKey = process.env.APIKEY
 
+const apiKey = "API key required"
 
 const fruitForm = document.querySelector('#fruitForm')
 const fruitList = document.querySelector('#fruitSection ul')
 const calorieCount = document.querySelector('#calorieCount')
-
+const createForm = document.querySelector('#createForm')
+const deleteForm = document.querySelector('#deleteForm')
 
 let calories = 0
 
@@ -20,9 +22,9 @@ function addFruit(fruit) {
     const li = document.createElement('li')
     const card = document.createElement('div')
     const img = document.createElement('img')
-    let randomNum = Math.floor(Math.random()*40)
+    let randomNum = Math.floor(Math.random()*20)
 
-    fetch(`https://fruity-api-0y87.onrender.com/fruits/${fruit}`)
+    fetch(`https://fruit-api-0y87.onrender.com/fruits/${fruit}`)
         .then((response) => response.json())
         .then((data) => {
             li.textContent = JSON.stringify(data.nutritions)
@@ -50,12 +52,53 @@ fruitList.addEventListener('click', (e => {
 }
 ))
 
-//
+createForm.addEventListener('submit', createNewFruit)
 
-/*async function fetchFryutData(fruit) {
-    const res = await fetch(`api-url${fruit})
-    const data = await res.json()
-    addfruit(data)
-    catch{}
+async function createNewFruit (e) {
+    e.preventDefault()
+
+    const data = {name: e.target.createInput.value}
+    const options = {
+        method: "post",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(data)
+    }
+
+    const response = await fetch("https://fruit-api-0y87.onrender.com/fruits", options)
+
+    if(response.status === 201){
+        e.target.reset()
+        alert('a new fruit was added')
+    } else {
+        e.target.reset()
+        alert("That fruit already exists")
+    }
 }
-*/
+
+deleteForm.addEventListener('submit', deleteFruit)
+
+async function deleteFruit (e) {
+    e.preventDefault()
+
+    const data = {name: e.target.deleteInput.value}
+    const options = {
+        method: "delete",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(data)
+    }
+
+    const response = await fetch("https://fruit-api-0y87.onrender.com/fruits", options)
+
+    if(response.status === 200){
+        e.target.reset()
+        alert('a fruit was deleted')
+    } else {
+        e.target.reset()
+        alert("That fruit doesn't exist")
+    }
+}
+
